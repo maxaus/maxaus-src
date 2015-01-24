695cead5a76ff31396d80e7381884a4d6c495dec
Maxim Baev <m.k.baev@gmail.com>
2015-01-24 22:52:14 +0600

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name maxausApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the maxausApp
+ */
+angular.module('maxausApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
