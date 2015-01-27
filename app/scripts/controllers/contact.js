5bf9b472d2aeff6395aecf631abd3c88ebc2bd56
Maxim Baev <m.k.baev@gmail.com>
2015-01-27 10:34:12 +0600

    Embedded google form on contact page

--- app/scripts/controllers/contact.js
+++ app/scripts/controllers/contact.js
@@ -6,12 +6,5 @@
 angular.module("maxausApp").controller("ContactCtrl", ["$scope", "UtilsService", function ($scope, UtilsService) {
 
   UtilsService.markListItemAsSelected("contact");
-  $scope.contactForm = {
-    "type": "service"
-  };
-
-  $scope.submitContactForm = function() {
-    //TODO: submit to server
-  };
 
 }]);
